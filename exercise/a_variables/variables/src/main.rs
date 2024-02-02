24de2204a70862b9e1517bbb0fe4d7f81f0bcaa2
marwan <marwan.s.dev@gmail.com>
2024-02-02 16:13:09 -0500

    Complete extra challenge in exercise A

--- exercise/a_variables/variables/src/main.rs
+++ exercise/a_variables/variables/src/main.rs
@@ -2,10 +2,13 @@ const STARTING_MISSILES: i32 = 8;
 const READY_AMOUNT: i32 = 2;
 
 fn main() {
-    let mut missiles = STARTING_MISSILES;
-    let ready = READY_AMOUNT;
+    // Rust warns about unused variables. Use prefix _unused if intentional
+    // let unused = 0;
+    // Rust does not allow assignment to const
+    // STARTING_MISSILES = 2;
+
+    let (missiles, ready) : (i32, i32) = (STARTING_MISSILES, READY_AMOUNT);
     println!("Firing {} of my {} missiles...", ready, missiles);
 
-    missiles = missiles - ready;
-    println!("{} missiles left", missiles );
+    println!("{} missiles left", missiles-ready );
 }
